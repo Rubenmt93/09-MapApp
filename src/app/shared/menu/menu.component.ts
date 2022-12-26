0ff80cb5e2677013c9143c71a02d69afc1b693a9
Ruben Montes <rubenmontes330@gmail.com>
2022-12-26 23:44:48 +0000

    Menu de la aplicación

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+
+interface menuItem{
+  ruta:string
+  nombre:string
+
+}
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
+  styles: [` 
+  li{
+    cursor:pointer
+  }
+  `
   ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+  
+  menuItems: menuItem[] = [
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    {
+      ruta:'/mapas/fullscreen',
+      nombre:'FullScreen'
+    },
+    {
+      ruta:'/mapas/zoom-range',
+      nombre:'Zoom Range'
+    },
+    {
+      ruta:'/mapas/marcadores',
+      nombre:'Marcadores'
+    },
+    {
+      ruta:'/mapas/propiedades',
+      nombre:'Propiedades'
+    },
+  ]
+  
 }
+ 
