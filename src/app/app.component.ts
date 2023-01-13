ec17c83409da6d187260733e683a4a23a0a5c08f
Ruben Montes <rubenmontes330@gmail.com>
2023-01-13 19:01:52 +0000

    Acces Token de Maznera Global a la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl  from 'mapbox-gl'
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mapasApp';
+export class AppComponent implements OnInit {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  
+
+  title = 'mapasApp'
+  ngOnInit(): void {
+
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11'
+   });
+  }
 }
--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl  from 'mapbox-gl'
-import { environment } from '../../../../environments/environment';
-
-
-
 
 @Component({
   selector: 'app-full-screen',
@@ -23,8 +19,7 @@ export class FullScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
-    (mapboxgl as any).accessToken = environment.mapboxToken;
+    
     var map = new mapboxgl.Map({
       container: 'mapa',
       style: 'mapbox://styles/mapbox/streets-v11'
