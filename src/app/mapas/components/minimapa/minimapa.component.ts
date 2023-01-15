cc1143d44f66469f7277cf18cae11516a90d3033
Ruben Montes <rubenmontes330@gmail.com>
2023-01-15 12:40:18 +0000

    Creacion y uso del componente minimap para las tarjetas de las propiedades

--- src/app/mapas/components/minimapa/minimapa.component.ts
+++ src/app/mapas/components/minimapa/minimapa.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-minimapa',
   templateUrl: './minimapa.component.html',
   styles: [
+    `
+    div{
+      width:100%;
+      height:150px;
+      margin:0px;
+
+    }
+    `
   ]
 })
-export class MinimapaComponent implements OnInit {
+export class MinimapaComponent implements AfterViewInit {
 
+
+  @Input() lngLat: [number,number] = [0,0]
+  @ViewChild('mapa') divMapa!: ElementRef;
+  
   constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    var mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive:false
+   });
+   new mapboxgl.Marker().setLngLat( this.lngLat).addTo(mapa)
   }
 
+  
+
 }
+ 
